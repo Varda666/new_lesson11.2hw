c7f4f362cc465bce05e0c8177e4578084743ba2c
Varda666 <oksana.aeshina666@gmail.ru>
2023-06-06 22:16:50 +0300

    add second func and docstrings str_func.py

--- str_func.py
+++ str_func.py
@@ -1,4 +1,9 @@
 
 s = input()
 def uppersymbols(s):
+    """Делает все буквы заглавными"""
     print(s.upper())
+
+def upper_first_symbol():
+    """ Делает первую букву заглавной"""
+    print(s.capitalize())
